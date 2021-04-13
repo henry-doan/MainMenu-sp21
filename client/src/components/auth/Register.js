54036cbb9e7fac5b7764db27d232f1db858921b5
Britt <Bargyle1@gmail.com>
2021-04-12 21:13:32 -0600

    login and register fix

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -29,7 +29,7 @@ const Register = ({ handleRegister, history }) => {
           name='email'
           value={user.email}
           placeholder='Email'
-          onChange={(e, { value }) => setUser({ ...user, email: value })}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
         />
         <input
           label="Password"
@@ -38,7 +38,7 @@ const Register = ({ handleRegister, history }) => {
           value={user.password}
           placeholder='Password'
           type='password'
-          onChange={(e, { value }) => setUser({ ...user, password: value })}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
         />
         <input
           label="Password Confirmation"
@@ -47,7 +47,7 @@ const Register = ({ handleRegister, history }) => {
           value={user.passwordConfirmation}
           placeholder='Password Confirmation'
           type='password'
-          onChange={(e, { value }) => setUser({ ...user, passwordConfirmation: value })}
+          onChange={(e) => setUser({ ...user, passwordConfirmation: e.target.value })}
         />
         <div>
           <button type='submit'>Submit</button>
