82f3d5116c6be021c327898e23a3a5d5c1b3ff9c
LuLu DeHaan <luludehaan@gmail.com>
2021-04-14 12:53:38 -0700

    working navbars, logout, register page styling

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,6 +1,22 @@
-const Home = () => (
-  <header as="h3" textAlign="center">Home</header>
-)
+import { IMG, HomeImgCont, HomeIMG } from "../shared/StyledComponents";
+import { Card } from "react-bootstrap";
+import Logo from "../images/Logo.png";
 
+const Home = () => (
+  <>
+    <HomeImgCont>
+      <HomeIMG src={Logo} alt="MainMenu Logo" fluid />
+      <h3>My Menus</h3>
+      <Card style={{ width: "15rem" }}>
+        <Card.Img variant="top" src={Logo} />
+        <Card.Body>
+          <Card.Title>Restaurant</Card.Title>
+        </Card.Body>
+      </Card>
+      <br></br>
+      <h3>My Favorites</h3>
+    </HomeImgCont>
+  </>
+);
 
 export default Home;
--- client/src/components/shared/Navigator.js
+++ client/src/components/shared/Navigator.js
@@ -1,28 +1,56 @@
 import React from 'react';
-import { Navbar, Nav,NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import LightTheme from '../../themes/Light';
-import {useContext}from 'react';
-import {Link,useHistory} from "react-router-dom"
-import { AuthContext } from '../../providers/AuthProvider'
+import { Link, withRouter } from "react-router-dom"
+import { AuthConsumer } from '../../providers/AuthProvider'
 
 const Navigator = ({theme, user, handleLogout, location, history }) => {
 
-  return (
-    
-  <Navbar collapseOnSelect expand="lg" bg={theme == LightTheme ? "light":"dark"} variant={theme == LightTheme ? "light":"dark"} >
-  <Navbar.Brand href="/">MainMenu</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-   
+    if (user) {
+      return (
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg={theme == LightTheme ? "light" : "dark"}
+          variant={theme == LightTheme ? "light" : "dark"}
+        >
+          <Navbar.Brand href="/">MainMenu</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="justify-content-end">
+              <Nav.Link onClick={() => handleLogout(history)}>Logout</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
       
-    <Nav className="justify-content-end">
-     <Nav.Link href="/register">Register</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+        </Navbar>
+    
+      );
+    } else {
+      return (
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg={theme == LightTheme ? "light" : "dark"}
+          variant={theme == LightTheme ? "light" : "dark"}
+        >
+          <Navbar.Brand href="/">MainMenu</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="justify-content-end">
+              <Nav.Link href="/register">Register</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      );
+    }
+  };
 
-  )
-}
 
 
-export default Navigator;
+const ConnectedNavigator = (props) => (
+  <AuthConsumer>{(auth) => <Navigator {...props} {...auth} />}</AuthConsumer>
+);
+
+
+export default withRouter(ConnectedNavigator);
