6fe857330b0265cb727d11f349f556a48ddad7a0
Sky B <skyburn5616@gmail.com>
2021-04-28 18:53:32 -0700

    faker issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'
+
 
 rimages = ['https://images.unsplash.com/photo-1589459985166-966c72e767d9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudHN8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
 'https://images.unsplash.com/photo-1612204021719-fa46ad9bd805?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzdGF1cmFudHN8ZW58MHwyfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
